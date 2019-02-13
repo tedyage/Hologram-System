cb2c60921f0fc124bd757eea387d574ff8041dde
tedyage <tedyage@sina.com>
2019-02-13 13:47:18 +0800

    coding hologram_3.0.js

--- static/js/hologram_v3.0.js
+++ static/js/hologram_v3.0.js
@@ -1,9 +1,30 @@
 var vm = new Vue({
-    el:'body',
+    el:'#scene',
     data:{
         width:window.innerWidth,
         height:window.innerHeight,
         scene:null,
-
+        lights:[]
+    },
+    methods:{
+        //初始化Scene
+        init_Scene:function(){
+            this.scene = new THREE.Scene();
+        },
+        //初始化光照
+        init_Lights:function(){
+            var ambientLight = new THREE.AmbientLight(0xf0f0f0,1.0);
+            var directionalLight = new THREE.DirectionalLight(0xffffff,0.6);
+            lights = [ambientLight,directionalLight];
+            for(var light of lights){
+                if(light)
+                    this.scene.add(light);
+            }
+        },
+    },
+    created:function(){
+        this.init_Scene();
+        this.init_Lights();
+        console.log(this.scene);
     }
 })
