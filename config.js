5f14a9aafe3a931f80d10373a642f6298119f105
tedyage <tedyage@sina.com>
2019-02-19 13:40:43 +0800

    add sequelize

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports={
+    database:'hologram',   //数据库名
+    username:'root',       //连接用户名
+    password:'a1b2c3d4',   //连接密码
+    host:'localhost',      //连接服务器名
+    port:3306              //连接服务器端口号
+}
